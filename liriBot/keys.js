ccbdde2b0d45e08de864f53e7a13bc48ffba9aeb
nateodes <nateodes@gmail.com>
2017-07-23 22:22:23 -0600

    working commit, finished

---
+++ liriBot/keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '3a6931CQTXEiDu1SGbiZGLM8s',
+  consumer_secret: 'YnkQwX99fUhzHtidHUl2BFwmAHv1ly2IsEQQ8Y10eCqwZYADHA',
+  access_token_key: '887423395910098946-ZuPB33DFznRosb7XjluC6B6YvCxFh1m',
+  access_token_secret: 'SMMYjWAIBC3OfAGf3QgZ8maVF5cVJZwQrjvXO7KVmDkVD'
+}
+
+exports.spotifyKeys = {
+	client_ID: '80a6b0be3e4c4305ab0b89929b00efb1',
+  	client_secret: 'fb5fe83fe42f46a7a21cf9b3037c97e3'
+
+}
+
+exports.omdbApiKey = {
+	API_key:"40e9cece"
+}
